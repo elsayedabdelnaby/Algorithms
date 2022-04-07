ef0cdb78a66170bc4d2f297d5033cfed61a0733b
sayed.elaraby <sayed.elaraby@kickstartinteractive.net>
2022-04-07 11:52:16 +0200

    handle Selection Sort Algorithm

---
+++ Sorting/SelectionSort.php
@@ -0,0 +1,24 @@
+<?php
+
+class SelectionSort
+{
+    public static function sort($array = []): array
+    {
+        $length = count($array);
+        for($i = 0; $i < $length; $i++)
+        {
+            $min_value_index = $i;
+            for($j = $i + 1; $j < $length; $j++)
+            {
+                if($array[$min_value_index] > $array[$j])
+                {
+                    $min_value_index = $j;
+                }
+            }
+            $temp = $array[$i];
+            $array[$i] = $array[$min_value_index];
+            $array[$min_value_index] = $temp;
+        }
+        return $array;
+    }
+}
